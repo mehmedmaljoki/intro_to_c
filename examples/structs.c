8463a6bb143774c1ae02cdfaca9623d4428a3c35
Mehmed Maljoki <mehmed.maljoki@porscheinformatik.com>
2024-11-08 22:46:45 +0100

    add structs.c

--- examples/structs.c
+++ examples/structs.c
@@ -1,4 +1,5 @@
 #include <stdbool.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -18,6 +19,26 @@ struct student {
     bool is_full_time;
     long double loan_outstanding;
 };
+
+// not aligned; dont use without good reason
+#pragma pack(push, 1)
+struct new_student {
+    char *name;
+    int id;
+    char initials[3];
+    double gpa;
+    int credit_hours;
+};
+#pragma pack(pop)
+
+struct good_alignment_student {
+    char *name;
+    double gpa;
+    int id;
+    int credit_hours;
+    char initials[3];
+};
+#pragma pack(pop)
 
 void student_report(struct student *s) {
     // s->name => (*s).name
@@ -66,5 +87,22 @@ int main() {
 
     student_report(&zero_student);
 
+    puts("++++++++++++++++++++++++++");
+
+    printf("Size of new_student: %zu\n", sizeof(struct new_student));
+    printf("offset of name: %zu\n", offsetof(struct new_student, name));
+    printf("offset of id: %zu\n", offsetof(struct new_student, id));
+    printf("offset of initials: %zu\n", offsetof(struct new_student, initials));
+    printf("offset of gpa: %zu\n", offsetof(struct new_student, gpa));
+    printf("offset of credit_hours: %zu\n", offsetof(struct new_student, credit_hours));
+
+    puts("++++++++++++++++++++++++++");
+
+    printf("Size of good_alignment_student: %zu\n", sizeof(struct good_alignment_student));
+    printf("offset of name: %zu\n", offsetof(struct good_alignment_student, name));
+    printf("offset of id: %zu\n", offsetof(struct good_alignment_student, id));
+    printf("offset of initials: %zu\n", offsetof(struct good_alignment_student, initials));
+    printf("offset of gpa: %zu\n", offsetof(struct good_alignment_student, gpa));
+    printf("offset of credit_hours: %zu\n", offsetof(struct good_alignment_student, credit_hours));
     return EXIT_SUCCESS;
 }
