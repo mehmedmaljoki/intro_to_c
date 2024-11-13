9505152567a19f555b4464610fffb64db8c4e521
Mehmed Maljoki <mehmed.maljoki@porscheinformatik.com>
2024-11-13 15:10:29 +0100

    finish fix errors

--- exercises/fixErrors/err1.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main(int arg, char **argv) {
-  printf("Hello World\n");
-  return 0;
-}
--- exercises/fixErrors/err2.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char **argv) {
-  char *p = "Hello\n";
-  char **p2 = &p;
-  printf("Das ist der pointer%p\n", *p2);
-  return 0;
-}
